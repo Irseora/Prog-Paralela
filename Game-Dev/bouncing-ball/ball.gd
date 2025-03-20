f2d14e7ca383c356fbf8f82d9ba835dd4ce4d972
Irseora <irseora@gmail.com>
2025-03-20 14:09:13 +0200

    readme

--- Game-Dev/bouncing-ball/ball.gd
+++ Game-Dev/bouncing-ball/ball.gd
@@ -1,21 +1,21 @@
 extends RigidBody2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
 func _on_death_body_entered(body: Node2D) -> void:
 	$"../Label".visible = true;
 	$"../Button".visible = true;
-	# get_tree().reload_current_scene();
-
 
 func _on_button_pressed() -> void:
 	get_tree().reload_current_scene();
+
+var collision_count = 0;
+func _on_body_entered(body):
+	collision_count += 1;
+	$"../Label2".text = "Bounced %d times" % collision_count;
